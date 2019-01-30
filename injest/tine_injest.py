b9be7260eab3d0bf0859a968e6b7370c644599e5
Ilya <ia@eduroam-0369.desy.de>
2019-01-30 11:12:08 +0100

    tine inhest

--- injest/tine_injest.py
+++ injest/tine_injest.py
@@ -14,9 +14,19 @@ def cb(id,cc,d):
 lid=pt.attach(address='/PETRA/Idc/Buffer-0',property='I',callback=cb)
 '''
 
+data = {}
 
-h=pt.history(address='/PETRA/HISTORY/keyword',property='CurDC',depth='2400hours', stop='30.11.2018 13:00:00')
+d=pt.list(context='PETRA', server='HISTORY')
+temp_channels = [t for t  in d['properties'] if t.lower().find('temp') >= 0]
+
+for i in len(temp_channels):
+    h=pt.history(address='/PETRA/HISTORY/keyword',property=temp_channels[i],depth='2400hours', stop='30.11.2018 13:00:00')
+
+data[temp_channels[i]] = {}
+
+
+#h=pt.history(address='/PETRA/HISTORY/keyword',property='CurDC',depth='2400hours', stop='30.11.2018 13:00:00')
 
 [datetime.utcfromtimestamp(t[1]) for t in h]
 
-json.dump(data, open('test.json','w'))
+json.dump(data, open('injest.json','w'))
