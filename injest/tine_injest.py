e9fa3c6d71c06bd11d96879dc9f1d9b3a9d30c8c
Ilya <ia@eduroam-1483.desy.de>
2019-03-11 14:54:23 +0100

    tine channels

--- injest/tine_injest.py
+++ injest/tine_injest.py
@@ -1,48 +1,86 @@
 # read archive channels from tine and prepare .json files to be later injested by the db
-import PyTine as pt
+#import PyTine as pt
 from datetime import datetime
 import json
 import sys
 
-'''
-# attaching listener
-def cb(id,cc,d):
-    print(d['timestamp'])
-    print(d['data'])
+
+def get_device_list(property):
+    d=pt.list(context='PETRA', server='HISTORY', property=property)
+    return d['devices']
+
+def get_value(property, device, ,depth, stop):
+    h=pt.history(address='/PETRA/HISTORY/'+device,property=property, depth=depth, stop=stop)
+    return h
+
+# tine has device-property paradigm
+# need to map it to relevant higher-level subsystems and parameters
+# Temperature
+# Alignment
+# Alignment->Sensor->X
+# Alignment->Sensor->Y
+# Beam
+# beam->Orbit
+# Beam->Qx, Qy
+# Beam->ex, ey
+# Beam->current
+# Beam->nBunches (fillPattern)
+# Beam
 
 
-lid=pt.attach(address='/PETRA/Idc/Buffer-0',property='I',callback=cb)
-'''
+#PETRA/HISTORY/T_PXN_X/BODENPLATTENMESSUNG
 
-data = {}
+#T_PXN bedeutet: In Strahlrichtung vom Tunnel nach PXN, also NR60.
+#PXN_T bedeutet: Von PXN in den Tunnel, also NR139
+#T_PXE bedeutet: In Strahlrichtung vom Tunnel nach PXE, also OR60.
+#PXE_T bedeutet: Von PXE in den Tunnel, also NR139
+#Das sind die Enden der Bodenplatte in den neuen Experimentierhallen.
 
-d=pt.list(context='PETRA', server='HISTORY',property='Temps.Magnets')
-#temp_channels = [t for t  in d['properties'] if t.lower().find('temp') >= 0]
-devices = d['devices']
-import time
+#Koordinaten:
+#x ist horizontal, positiv nach ringaußen
+#y ist vertikal, positiv nach oben
+#z ist longitudinal, positiv in Strahlrichtung.
 
-#h=pt.history(address='/PETRA/HISTORY/keyword',property=temp_channels[0],depth='2400hours', stop='30.11.2018 13:00:00')
-#data[temp_channels[0]] = h
-#time.sleep(2)
-#h=pt.history(address='/PETRA/HISTORY/keyword',property=temp_channels[1],depth='2400hours', stop='30.11.2018 13:00:00')
-#data[temp_channels[1]] = h
+#Das gleiche gibt es als
+#PETRA/HISTORY/T_SWL_X/BODENSEGMENTMESSUNG  (...SEGMENT...!)
 
-for ch in devices:
-    print('reading channel: ' + ch)
-    try:
-        time.sleep(0.1)
-        h=pt.history(address='/PETRA/HISTORY/'+ch,property='Temps.Magnets',depth='2400hours', stop='30.12.2018 13:00:00')
-        time.sleep(0.1)
-        data[ch] = {'data':h}
-        print('ok')
-    except Exception as e:
-        print('No channel data in archive')
-        print(e)
+#T_SWL bedeutet: In Strahlrichtung vom Tunnel nach SWL, also SWL38.
+#SWR_T bedeutet: In Strahlrichtung von SWR in den Tunnel, also SWR38.
+#Dies sind die beiden Messungen an 'normalen' Gebäudefugen.
 
-data['tags'] = ['petra','archive','history']
 
-#h=pt.history(address='/PETRA/HISTORY/keyword',property='CurDC',depth='2400hours', stop='30.11.2018 13:00:00')
+#DataFrame
 
-#[datetime.utcfromtimestamp(t[1]) for t in h]
+def get_data_frame(date, depth, sampling):
+    dataFrame = {}
+    dataFrame['Temperature'] = {}
+    property = 'Temps.Magnets'
+    devices = get_device_list(property)
+    for dev in devices:
+        h=get_value(propert=property, device=dev, depth=depth, stop=date + ' 13:00:00')
+        dataFrame['Temperature'][dev] = h
 
-json.dump(data, open(sys.argv[1],'w'))
+    dataFrame['Alignment'] = {}
+    property = 'BODENSEGMENTMESSUNG'
+    devices = get_device_list(property)
+    for dev in devices:
+        h=get_value(propert=property, device=dev, depth=depth, stop=date + ' 13:00:00')
+        dataFrame['Alignment'][dev] = h
+
+    property = 'BODENPLATTENMESSUNG'
+    devices = get_device_list(property)
+    for dev in devices:
+        h=get_value(propert=property, device=dev, depth=depth, stop=date + ' 13:00:00')
+        dataFrame['Alignment'][dev] = h
+
+
+    dataFrame['Beam'] = {}
+    dataFrame['Beam']['Qx'] = {}
+    dataFrame['Beam']['Qx']['unit'] = 'Hz'
+    dataFrame['Beam']['Qx']['value'] = 0.0
+
+    return dataFrame
+
+frame = get_data_frame('31.12.2018','48hours','')
+#print(frame)
+json.dump(dataFrame, open(sys.argv[1],'w'))
